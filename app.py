9029c48fedd3d28c198c1e9765ca29fcfef118c6
Lei Chen <lei.chen@liulishuo.com>
2018-05-01 10:55:43 -0400

    Can call openface in docker to run face landmark detection.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 #
 #
 import os
+from subprocess import call
 
 from flask import Flask, render_template, request, send_from_directory, redirect, url_for
 
@@ -12,8 +13,12 @@ app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.debug = True
 
+PROCESSED_FOLDER = 'images/processed'
+print(PROCESSED_FOLDER)
+
+app.config['PROCESSED_FOLDER'] = PROCESSED_FOLDER
+app.debug = True
 
 @app.route('/')
 def hello_world():
@@ -30,10 +35,36 @@ def upload_file():
 
 @app.route('/show/<filename>')
 def uploaded_file(filename):
-    filename = 'http://127.0.0.1:5000/uploads/' + filename
-    return render_template('home.html', filename=filename)
+    # filename = '/uploads/' + filename
+    return render_template('home.html', img_org=filename)
 
 
+@app.route('/openface/<filename>')
+def run_openface(filename):
+    '''
+
+    :param filename:
+    :return:
+    '''
+    with open("./tmp/output.log", 'a') as output:
+        call(
+            "docker run -v /Users/lchen/Documents/GitHub/cv_flask/images:/data -w \'/data\' -i -t openface_v1.0 /opt/OpenFace/build/bin/FaceLandmarkImg -f " + filename,
+            shell=True, stdout=output, stderr=output)
+    processed_img = filename.replace('jpeg', 'jpg')
+    return redirect(url_for('processed_file', filename=processed_img))
+
+
+@app.route('/result/<filename>')
+def processed_file(filename):
+    return render_template('home.html', img_proc=filename)
+
+
+# need this for showing images
 @app.route('/uploads/<filename>')
 def send_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
+
+
+@app.route('/images/processed/<filename>')
+def send_file2(filename):
+    return send_from_directory(PROCESSED_FOLDER, filename)
